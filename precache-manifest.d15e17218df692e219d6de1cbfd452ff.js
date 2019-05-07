4f0865e56d5806470490416c248ecbb95f5e0a3f
war12oc <ganglovesee@gmail.com>
2019-05-07 11:19:34 +0700

    Updates

---
+++ precache-manifest.d15e17218df692e219d6de1cbfd452ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124ada63030d3583fff2c36562649faf",
+    "url": "/testcallapi/index.html"
+  },
+  {
+    "revision": "544b6423edfb8cea2bf6",
+    "url": "/testcallapi/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/testcallapi/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "544b6423edfb8cea2bf6",
+    "url": "/testcallapi/static/js/main.5ee22a69.chunk.js"
+  },
+  {
+    "revision": "00767ced018e8c80ba02",
+    "url": "/testcallapi/static/js/runtime~main.7e1ca7d7.js"
+  }
+]);
